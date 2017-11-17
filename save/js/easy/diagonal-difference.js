295c58524c56b266b629c99899aaef70f69249b0
Kevin Smith <ksjazzguitar@yahoo.com>
2017-11-16 22:25:36 -0800

    initial ...

---
+++ save/js/easy/diagonal-difference.js
@@ -0,0 +1,41 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function absDiagDiff(n, arr) {
+    var diff = 0;
+
+    for (var i=0; i<n; i++) {
+        diff += arr[i][i] - arr[i][(n-1)-i]
+    }
+
+    return Math.abs(diff);
+}
+
+function main() {
+    var n = parseInt(readLine());
+    var a = [];
+    for(a_i = 0; a_i < n; a_i++){
+       a[a_i] = readLine().split(' ');
+       a[a_i] = a[a_i].map(Number);
+    }
+    console.log(absDiagDiff(n, a));
+}
